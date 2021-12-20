b9cb6ca1e6588f2fde24106b865d38a58696cf27
luisa <luisa.ferreira@ufv.br>
2021-12-20 17:49:28 -0300

    views

---
+++ project/resources/views/site/principal.blade.php
@@ -0,0 +1,14 @@
+<h3>Principal Page</h3>
+
+<ul>
+    <li>
+        <a href="/"> Principal Page</a>
+    </li>
+    <li>
+        <a href="/sobre-nos">Sobre Nos Page</a>
+    </li>
+    <li>
+        <a href="/contato">Contato Page</a>
+    </li>
+
+</ul>
---
+++ project/resources/views/site/sobrenos.blade.php
@@ -0,0 +1,1 @@
+<h3>Sobre Nos Page</h3>
